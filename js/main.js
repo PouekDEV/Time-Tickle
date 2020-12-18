56dfccee911f547787f494f837a2e3c383450dc3
Jakub Starostecki <64737924+PouekDEV@users.noreply.github.com>
2020-12-18 08:31:10 +0100

    files

---
+++ js/main.js
@@ -0,0 +1,130 @@
+//ilości
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
+var days = 0;
+var tp = 0;
+var ts = 100;
+//ustawienia
+var update_rate = 50;
+//generatory
+var seconds_generator = 0;
+var minutes_generator = 0;
+var hours_generator = 0;
+//koszty
+var sg_cost = 0;
+var mg_cost = 10;
+var hg_cost = 10;
+document.getElementById("tp").innerHTML = "Time Points: " + tp;
+document.getElementById("ts").innerHTML = "Time Stability: " + ts +"%";
+document.getElementById("ms").innerHTML = update_rate +"ms"
+document.getElementById("seconds").innerHTML = seconds;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("days").innerHTML = days;
+document.getElementById("sg").innerHTML = "Stopwatches: " + seconds_generator;
+document.getElementById("mg").innerHTML = "Countdown timers: " + minutes_generator;
+document.getElementById("hg").innerHTML = "Hour meters: " + hours_generator;
+document.getElementById("sc").innerHTML = "Cost: " + sg_cost;
+document.getElementById("mc").innerHTML = "Cost: " + mg_cost;
+document.getElementById("hc").innerHTML = "Cost: " + hg_cost;
+setInterval(() =>{
+    seconds += seconds_generator * 0.5;
+    minutes += minutes_generator * 0.5;
+    hours += hours_generator * 0.5;
+    if(seconds >= 1000000 && minutes >= 100000 && hours >= 100000){
+        document.getElementById("finb").style.display = "block";
+    }
+    if(ts <= 0){
+        document.getElementById("ts").innerHTML = "You broke the time"
+        document.getElementById("bd").style.display = "none";
+        document.getElementById("bdd").style.display = "none";
+        document.getElementById("klep").style.display = "block";
+    }
+    else{
+        document.getElementById("finb").style.display = "none";
+        document.getElementById("klep").style.display = "none";
+        document.getElementById("ts").innerHTML = "Time Stability: " + ts +"%";
+        if(seconds >= 60){
+            seconds = 0;
+            minutes +=1;
+        }
+        if(minutes >= 60){
+            minutes = 0;
+            hours += 1;
+        }
+        if(hours >= 24){
+            hours = 0;
+            days += 1;
+        }
+        if(days >= 365){
+            days = 0;
+            tp += 1;
+        }
+    }
+    document.getElementById("tp").innerHTML = "Time Points: " + tp;
+    document.getElementById("seconds").innerHTML = seconds;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("sg").innerHTML = "Seconds Generators: " + seconds_generator;
+    document.getElementById("mg").innerHTML = "Minutes Generators: " + minutes_generator;
+    document.getElementById("hg").innerHTML = "Hours Generators: " + hours_generator;
+    document.getElementById("sc").innerHTML = "Cost: " + sg_cost + "s";
+    document.getElementById("mc").innerHTML = "Cost: " + mg_cost + "Min";
+    document.getElementById("hc").innerHTML = "Cost: " + hg_cost + "H";
+},update_rate)
+function buysg(){
+    if(seconds >= sg_cost){
+        seconds_generator += 1;
+        sg_cost += 1;
+    }
+}
+function buymg(){
+    if(minutes >= mg_cost){
+        minutes_generator += 1;
+        mg_cost += 1;
+    }
+}
+function buyhg(){
+    if(hours >= hg_cost){
+        hours_generator += 1;
+        hg_cost += 1 
+    }
+}
+function damaget(){
+    if(tp >= 1){
+    ts -= tp * 1.5
+    //ilości
+    seconds = 0;
+    minutes = 10;
+    hours = 10;
+    days = 0;
+    tp = 0;
+    //ustawienia
+    update_rate = 50;
+    //generatory
+    seconds_generator = 0;
+    minutes_generator = 0;
+    hours_generator = 0;
+    //koszty
+    sg_cost = 0;
+    mg_cost = 10;
+    hg_cost = 10;
+    }
+}
+function sike(){
+    location.reload();
+}
+function updaterate(){
+    if(update_rate == 50){
+        update_rate = 200;
+    }
+    else if(update_rate == 200){
+        update_rate = 100;
+    }
+    else if(update_rate == 100){
+        update_rate = 50;
+    }
+    document.getElementById("ms").innerHTML = update_rate +"ms"
+}
